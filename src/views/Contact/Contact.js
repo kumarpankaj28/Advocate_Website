66c750c06aba3bf86ef64646b2117f66a4c2fc7f
Pankaj Kumar <developer.osizone21@gmail.com>
2021-12-06 09:41:31 +0530

    contact JS update

--- src/views/Contact/Contact.js
+++ src/views/Contact/Contact.js
@@ -1,23 +1,45 @@
-import React from "react";
-import Faq from '../Faq/Faq';
-// import { useState } from 'react';
+import React, { useState } from "react";
+import Faq from "../Faq/Faq";
+import { db } from "../../config";
 
 export default function Contact() {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    massage: "",
+  });
 
+  let name, value;
+  const getUserData = (event) => {
+    name = event.target.name;
+    value = event.target.value;
 
-
-
-
-
-
-
+    setUser({ ...user, [name]: value });
+  };
+  
+  const handleSubmit = async (e) => {
+    // e.preventDefault();
+    // db.firestore()
+    //   .collection('contact')
+    //   .add({
+    //     name: name,
+    //     email: email,
+    //     massage:massage,
+    //   });
+    // name('');
+    // value('');
+    // setTimeout(() => {
+    // value('')
+    // }, 2000)
+  }
+   
 
 
 
   return (
     <div>
-      <Faq/>
-      <div className="py-4 lg:py-8  relative">
+      <Faq />
+      <div className="py-4 lg:py-8  relative ">
         <img
           src="https://cdn.tuk.dev/assets/templates/radian/Back_Image.png"
           className="h-2/5 lg:h-full w-full lg:w-1/2 absolute inset-0 object-cover object-center xl:block hidden"
@@ -73,7 +95,10 @@ export default function Contact() {
               </div>
             </div>
             <div className="w-full lg:w-1/2   xl:pt-10 lg:pl-24">
-              <div className="flex flex-col items-start xl:justify-start 2xl:justify-end xl:px-0 px-4">
+              <form
+                className="flex flex-col items-start xl:justify-start 2xl:justify-end xl:px-0 px-4"
+                method="POST"
+              >
                 <h1 className="text-4xl md:text-5xl lg:text-7xl font-bold tracking-wider text-indigo-700">
                   Let’s Talk
                 </h1>
@@ -86,7 +111,12 @@ export default function Contact() {
                     <input
                       className="mt-3 text-base border-2 w-11/12 lg:w-full xl:w-10/12 hover:border-indigo-600 focus:border-indigo-600 focus:outline-none border-black py-5 pl-4 text-gray-800"
                       type="text"
+                      name="name"
                       placeholder="Justin Timberlake"
+                      value={user.name}
+                      onChange={getUserData}
+                      autoComplete="off"
+                      required
                     />
                   </div>
                   <div className="mt-4 md:mt-8">
@@ -96,7 +126,12 @@ export default function Contact() {
                     <input
                       className="mt-3 text-base border-2 w-11/12 lg:w-full xl:w-10/12 hover:border-indigo-600 focus:border-indigo-600 focus:outline-none border-black py-5 pl-4 text-gray-800"
                       type="email"
+                      name="email"
                       placeholder="example@mail.com"
+                      value={user.email}
+                      onChange={getUserData}
+                      autoComplete="off"
+                      required
                     />
                   </div>
                   <div className="mt-4 md:mt-8">
@@ -106,17 +141,24 @@ export default function Contact() {
                     <textarea
                       className="mt-3 text-base border-2 w-11/12 lg:w-full xl:w-10/12 resize-none hover:border-indigo-600 focus:border-indigo-600 focus:outline-none border-black xl:h-40 py-5 pl-4 text-gray-800"
                       type="text"
+                      name="massage"
                       placeholder="Write us something..."
-                      defaultValue={""}
+                      value={user.massage}
+                      onChange={getUserData}
+                      autoComplete="off"
+                      required
                     />
                   </div>
                   <div className="py-5">
-                    <button className="py-3 md:py-5 px-5 md:px-10 bg-gray-900 text-white hover:opacity-90 ease-in duration-150 text-sm md:text-lg tracking-wider font-semibold">
+                    <button
+                      className="py-3 md:py-5 px-5 md:px-10 bg-gray-900 text-white hover:opacity-90 ease-in duration-150 text-sm md:text-lg tracking-wider font-semibold"
+                      onClick={handleSubmit}
+                    >
                       Send
                     </button>
                   </div>
                 </div>
-              </div>
+              </form>
             </div>
           </div>
           {/* <iframe  title="my-map"src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d224346.4812958237!2d77.06889891452637!3d28.52728034310044!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfd5b347eb62d%3A0x52c2b7494e204dce!2sNew%20Delhi%2C%20Delhi!5e0!3m2!1sen!2sin!4v1638190203547!5m2!1sen!2sin" width="600" height="450" allowfullscreen="" loading="lazy"></iframe> */}
@@ -124,4 +166,4 @@ export default function Contact() {
       </div>
     </div>
   );
-}
+};
